37462ae39871f8855e4357cf71c8549f89416b7c
Ayush Agrawal <07ayush0707@gmail.com>
2023-04-23 22:29:58 +0530

    Solving the bar chart problem

--- arrays/8.BarChart.java
+++ arrays/8.BarChart.java
@@ -24,6 +24,38 @@ class BarChart {
         System.out.println(Arrays.toString(arr));
     }
 
+    public static int maxValue(int[] arr) {
+
+        int maxValue = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxValue = ((maxValue < arr[i]) ? maxValue = arr[i] : maxValue);
+        }
+        return maxValue;
+
+    }
+
+    public static void pattern(int[] arr, int maxValue) {
+
+        int diff = maxValue;
+
+        for (int i = 0; i < maxValue; i++) {
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if (diff - arr[j] <= 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            diff -= 1;
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
@@ -37,6 +69,11 @@ class BarChart {
 
             System.out.println("Original Array : ");
             display(arr);
+
+            int maxValue = maxValue(arr);
+
+            System.out.println("Pattern: ");
+            pattern(arr, maxValue);
 
         }
 
